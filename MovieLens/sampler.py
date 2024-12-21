da2dfe0b6edbba7aff11cc5d71c8551f15d74735
icf <119780106+15505197860@users.noreply.github.com>
2024-12-21 20:47:34 +0800

    Update sampler.py

--- MovieLens/sampler.py
+++ MovieLens/sampler.py
@@ -10,44 +10,46 @@ def random_neq(l, r, s):
     return t
 
 
+#进行目标交互的采集，正负样本的采样
 def sample_function(user_train, Beh, Beh_w, usernum, itemnum, batch_size, maxlen, result_queue, SEED):
     def sample():
         recency_alpha = 0.5
         user = np.random.randint(1, usernum + 1)
         while len(user_train[user]) <= 1: user = np.random.randint(1, usernum + 1)
 
-        seq = np.zeros([maxlen], dtype=np.int32)
-        pos = np.zeros([maxlen], dtype=np.int32)
-        neg = np.zeros([maxlen], dtype=np.int32)
+        seq = np.zeros([maxlen], dtype=np.int32) #用户的行为序列
+        pos = np.zeros([maxlen], dtype=np.int32) #与用户行为序列相对应的正样本
+        neg = np.zeros([maxlen], dtype=np.int32)  #负样本
         recency = np.zeros([maxlen], dtype=np.float32)
-        nxt = user_train[user][-1]
-        idx = maxlen - 1
+        nxt = user_train[user][-1]  #获取用户最后一个行为
+        idx = maxlen - 1   #从后往前，按时间顺序
 
-        ts = set(user_train[user])
-        for i in reversed(user_train[user][:-1]):
-            seq[idx] = i
-            pos[idx] = nxt
-            recency[idx] = recency_alpha**(maxlen-idx) 
+        ts = set(user_train[user])  #set: 可以快速判断某个行为是否在用户的历史行为中出现过，这在后续生成负样本时会用到
+        #两个指针 i 和 nxt
+        for i in reversed(user_train[user][:-1]):   #除最后一个的用户行为i 反转（按时间顺序）
+            seq[idx] = i   
+            pos[idx] = nxt   #从最后一个行为开始
+            recency[idx] = recency_alpha**(maxlen-idx) #第idx的行为的时间衰减
             #print('recency[idx]...', recency[idx])
-            if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)
-            nxt = i
+            if nxt != 0: neg[idx] = random_neq(1, itemnum + 1, ts)  #生成一个负样本
+            nxt = i    
             idx -= 1
             if idx == -1: break
         #print(abc)
-        seq_cxt = list()
+        seq_cxt = list()  #用户交互的上下文信息
         pos_cxt = list()
         pos_weight = list()
         neg_weight = list()
 
         for i in seq :
-            seq_cxt.append(Beh[(user,i)])
+            seq_cxt.append(Beh[(user,i)])   #获取用户u和行为i对应的上下文信息
 
         for i in pos :
             pos_cxt.append(Beh[(user,i)])
 
         for i in pos :
-            pos_weight.append(Beh_w[(user,i)])
-            neg_weight.append(1.0)
+            pos_weight.append(Beh_w[(user,i)])  #损失函数中的α
+            neg_weight.append(1.0)    #β
 
         seq_cxt = np.asarray(seq_cxt)  
         pos_cxt = np.asarray(pos_cxt)    
